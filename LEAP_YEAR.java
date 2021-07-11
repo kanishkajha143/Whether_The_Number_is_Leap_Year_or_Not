974f09caab8fe12e4a5bb9d470f7f2c2e7856203
kanishka jha <75806329+kanishkajha143@users.noreply.github.com>
2021-07-11 11:47:43 +0530

    Create LEAP_YEAR.java

---
+++ LEAP_YEAR.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Demo {
+    public static void main(String[] args) {
+
+    	int year;
+    	Scanner scan = new Scanner(System.in);
+    	System.out.println("Enter the Year:");
+    	year = scan.nextInt();
+    	scan.close();
+        boolean isLeap = false;
+
+        if(year % 4 == 0)
+        {
+            if( year % 100 == 0)
+            {
+                if ( year % 400 == 0)
+                    isLeap = true;
+                else
+                    isLeap = false;
+            }
+            else
+                isLeap = true;
+        }
+        else {
+            isLeap = false;
+        }
+
+        if(isLeap==true)
+            System.out.println(year + " is a LEAP YEAR.");
+        else
+            System.out.println(year + " is not a LEAP YEAR.");
+    }
+}
